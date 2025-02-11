8da4a7770343228cd51bbfcb47d704088f27795a
zljxl <57012227+Pedroozl@users.noreply.github.com>
2025-02-11 10:49:44 -0300

    Add files via upload

---
+++ src/main/java/com/mycompany/mavenproject1/Calculadora.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.mavenproject1;
+
+/**
+ *
+ * @author alunolab08
+ */
+public class Calculadora {
+    
+    public static int soma(int a, int b) {
+        int s = 0;
+        return s;
+    }
+    
+    public static int subtracao(int a, int b) {
+        int s = 0;
+        return s;
+    }
+    
+    public static int multiplicacao(int a, int b) {
+        int s = 0;
+        return s;
+    }
+    
+    public static int divisao(int a, int b) {
+        int s = 0;
+        return s;
+    }
+}
---
+++ src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
@@ -0,0 +1,48 @@
+package com.mycompany.mavenproject1;
+
+import java.util.Scanner;
+
+public class Mavenproject1 {
+    public static void menuPrint() {
+        Scanner sc1 = new Scanner(System.in);
+        
+        System.out.println("/=== Menu ===/");
+        System.out.println("[1] Adição");
+        System.out.println("[2] Subtração");
+        System.out.println("[3] Multi");
+        System.out.println("[4] Divisão");
+        System.out.println("[0] Sair");
+        System.out.println("Escolha uma opção: ");
+        
+        int opt = sc1.nextInt();
+        
+        System.out.flush();
+        
+        switch(opt) {
+            case 0:
+                System.exit(0);
+               break;
+            case 1: 
+                Calculadora.soma(0, 0);
+                break;
+            case 2: 
+                Calculadora.subtracao(0, 0);
+                break;
+            case 3: 
+                Calculadora.multiplicacao(0, 0);
+                break;
+            case 4: 
+                Calculadora.divisao(0, 0);
+                break;
+            default:
+                menuPrint();
+                break;
+        }
+    }
+    
+    
+    public static void main(String[] args) {
+        
+        menuPrint();
+    }
+}
